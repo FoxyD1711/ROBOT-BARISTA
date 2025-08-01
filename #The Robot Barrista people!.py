595a45825c13b7e904763ada067cb20196b863fc
FoxyD1711 <alexbritocollazo@gmail.com>
2025-08-01 19:20:26 -0400

    umm, Its a robot+Barrista
    yep thats it

---
+++ #The Robot Barrista people!.py
@@ -0,0 +1,24 @@
+#The Robot Barrista people!
+name=input('What is your name?\n')
+
+
+print('Hello ' +  name, 'Welcome To Python Playground\n\n\n\n')
+
+print('So....' + name + ' Right? Heres your menu for today\n')
+
+print('1. Tacos\n2. Burritos\n3. Nachos\n4. Quesadillas\n5. Enchiladas\n6. Fajitas\n7. Chilaquiles\n')
+
+Order=input('What would you like to order?\n')
+
+print('Exelent choice! Your ' + Order + ' will be ready in 5 minutes\n\n\n\n')
+
+print('What Drink would u like?\n')
+
+drink=input('Heres our options,\n 1.Soda\n2. Coffee\n3. lemonade\n')
+
+print('Thats my favorite drink!\n\n\n\n')
+
+print('Exelent Choice '+ name +'. Your ' + drink + ' and  your '+ Order + ' will be ready in 2 minutes')
+
+
+This is it where it all started BABY
